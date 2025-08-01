e834b0c62546ee5bd4fc7d54ad911e83a9c7738f
parvusdomus <parvusdomus@gmail.com>
2025-08-01 11:49:11 +0200

    back to v12

--- modules/preloadTemplates.js
+++ modules/preloadTemplates.js
@@ -2,5 +2,6 @@ export const preloadHandlebarsTemplates = async function () {
     const templatePaths = [
       "/systems/tricube/templates/actors/parts/general.html"
     ];
-        return loadTemplates(templatePaths);
+        //return loadTemplates(templatePaths);
+        return foundry.applications.handlebars.loadTemplates(templatePaths);
 };
